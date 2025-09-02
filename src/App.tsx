19a242ea32a399a5587cfcd0284b5326d20a879d
Jeevith <147390108+Jeevith08@users.noreply.github.com>
2025-09-02 23:11:24 +0530

    URGENT: Remove problematic redirect scripts and fix CSP issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -11,8 +11,6 @@ import HeartCursor from './components/HeartCursor';
 const queryClient = new QueryClient();
 
 const App = () => {
-  console.log('App component loaded, base path:', import.meta.env.BASE_URL);
-  
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
@@ -20,12 +18,12 @@ const App = () => {
           <Toaster />
           <Sonner />
           <HeartCursor />
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
